b8257dcdc9b1314ef773a5834003aadc23d0ece9
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-12-03 14:27:22 -0300

    funções createDAta e pushToList

--- airplane.c
+++ airplane.c
@@ -6,35 +6,89 @@
 
 
 // Struct para armazenar os dados dos aviões
+typedef struct{
+    char *empresa;
+    char *lugares;
+}Data;
 
 typedef struct{
-    char empresa[50];
-    char lugares[3];
+    Data *data;
+    struct No *next;
+}No;
+
+typedef struct{
+    No *head;
+    No *tail;
 }Airplane;
 
 
 
-// Manipula os dados dos aviões
 
-// Adiciona um novo avião
+// funções para manipular lista de avioes
 
-void addAirplane(Airplane *airplane, char *destine){
-    if(!airplane){
-        perror("Dados nulos");
-        exit(1);
+Data *createData(char *empresa, char *lugares){
+    if(!empresa || !lugares){
+        perror("erro em createData");
+        return NULL;
     }
 
-    FILE *file = fopen("airplaneM.txt", "a");
-    
-    if(!file){
-        perror("Erro ao abrir o arquivo");
-        exit(1);
+    Data *data = malloc(sizeof(Data));
+    if(!data){
+        perror("erro ao alocar memória");
+        free(data);
+        return NULL;
     }
 
-    fprintf(file, "\n%s\n%s", airplane->empresa, airplane->lugares);
-    fclose(file);
-    
+    data->empresa = malloc(strlen(empresa) + 1);
+    if(!data->empresa){
+        perror("erro ao alocar memória");
+        free(data->empresa);
+        free(data);
+        return NULL;
+    }
+
+    data->lugares = malloc(strlen(lugares) + 1);
+    if(!data->lugares){
+        perror("erro ao alocar memória");
+        free(data->empresa);
+        free(data->lugares);
+        free(data);
+        return NULL;
+    }
+
+    strcpy(data->empresa, empresa);
+    data->empresa[strlen(empresa)] = '\0';
+    strcpy(data->lugares, lugares);
+    data->lugares[strlen(lugares)] = '\0';
+
+    return data;
+
 }
+
+void pushToList(Airplane *airplane, No *data){
+   if(!airplane || !data){
+       perror("erro em pushToList");
+       exit(1);
+   }
+
+   No *new = malloc(sizeof(No));
+   if(!new){
+       perror("erro ao alocar memória");
+       exit(1);
+   }
+
+   new->data = data;
+   new->next = NULL;
+
+   if(!airplane->head){
+       airplane->head = new;
+       airplane->tail = new;
+   }else{
+       airplane->tail->next = new;
+       airplane->tail = new;
+   }
+}
+
 
 
 
