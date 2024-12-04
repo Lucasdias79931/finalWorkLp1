45d71a6e25472b95af3c6e8792a3bdc6b82224fb
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-12-03 21:07:00 -0300

    função push pronta

--- airplane.c
+++ airplane.c
@@ -6,20 +6,22 @@
 
 
 // Struct para armazenar os dados dos aviões
-typedef struct{
+typedef struct Data{
     char *tokey;
     char *empresa;
-    char *lugares;
+    char *lugaresMax;
+    char *disponiveis;
 }Data;
 
-typedef struct{
+typedef struct No{
     Data *data;
     struct No *next;
 }No;
 
-typedef struct{
+typedef struct Airplane{
     No *ini;
     No *end;
+    int size;
 }Airplane;
 
 
@@ -27,8 +29,8 @@ typedef struct{
 
 // funções para manipular lista de avioes
 
-Data *createData(char *empresa, char *lugares, char *tokey){
-    if(!empresa || !lugares || !tokey){
+Data *createData(char *empresa, char *lugaresMax, char *disponiveis, char *tokey){
+    if(!empresa || !lugaresMax || !tokey || !disponiveis){
         perror("erro em createData");
         return NULL;
     }
@@ -41,46 +43,55 @@ Data *createData(char *empresa, char *lugares, char *tokey){
         return NULL;
     }
 
+    data->tokey = malloc(strlen(tokey) + 1);
+    if(!data->tokey){
+        perror("erro ao alocar memória");
+        free(data->tokey);
+        free(data);
+        return NULL;
+    }
+
     data->empresa = malloc(strlen(empresa) + 1);
     if(!data->empresa){
         perror("erro ao alocar memória");
         free(data->empresa);
+        free(data->tokey);
         free(data);
         return NULL;
     }
 
-    data->lugares = malloc(strlen(lugares) + 1);
-    if(!data->lugares){
+    data->lugaresMax = malloc(strlen(lugaresMax) + 1);
+    if(!data->lugaresMax){
         perror("erro ao alocar memória");
+        free(data->lugaresMax);
         free(data->empresa);
-        free(data->lugares);
+        free(data->tokey);
         free(data);
         return NULL;
     }
 
-    strcpy(data->empresa, empresa);
-    data->empresa[strlen(empresa)] = '\0';
-    strcpy(data->lugares, lugares);
-    data->lugares[strlen(lugares)] = '\0';
-
-    data->tokey = malloc(strlen(tokey) + 1);
-    if(!data->tokey){
+    data->disponiveis = malloc(strlen(disponiveis) + 1);
+    if(!data->disponiveis){
         perror("erro ao alocar memória");
+        free(data->disponiveis);
+        free(data->lugaresMax);
         free(data->empresa);
-        free(data->lugares);
         free(data->tokey);
         free(data);
         return NULL;
     }
 
     strcpy(data->tokey, tokey);
-    data->tokey[strlen(tokey)] = '\0';
+    strcpy(data->empresa, empresa);
+    strcpy(data->lugaresMax, lugaresMax);
+    strcpy(data->disponiveis, disponiveis);
+    
     return data;
 
 }
 
 
-void pushToList(Airplane *airplane, No *data){
+void pushToList(Airplane *airplane, Data *data){
    if(!airplane || !data){
        perror("erro em pushToList");
        exit(1);
@@ -102,6 +113,8 @@ void pushToList(Airplane *airplane, No *data){
        airplane->end->next = new;
        airplane->end = new;
    }
+
+   airplane->size++;
 }
 
 void deleteInList(Airplane *airplane, char *tokey){
@@ -118,8 +131,9 @@ void deleteInList(Airplane *airplane, char *tokey){
             current = current->next;
 
             free(aux->data->tokey);
-            free(aux->data->lugares);
+            free(aux->data->lugaresMax);
             free(aux->data->empresa);
+            free(aux->data->disponiveis);
             free(aux->data);
             free(aux);
             printf("Aviao removido com sucesso\n");
@@ -134,13 +148,19 @@ void deleteInList(Airplane *airplane, char *tokey){
 void printList(Airplane *airplane){
 
     if(!airplane){
-        perror("Lista de avioes vazia");
+        perror("erro em printList");
+        return;
+    }
+
+    if(airplane->size == 0){
+        printf("Nenhum aviao cadastrado\n");
         return;
     }
 
     No *current = airplane->ini;
     while(current){
-        printf("%s %s %s\n", current->data->tokey, current->data->lugares, current->data->empresa);
+        printf("Tokey: %s\nlugaresMax: %s\nLugares disponiveis: %s\nEmpresa: %s\n", current->data->tokey, current->data->lugaresMax, current->data->disponiveis, current->data->empresa);
+        current = current->next;
     }
 
 }
@@ -160,7 +180,8 @@ void fromFileToList(Airplane *airplane, char *path){
     }
 
     char empresa[50];
-    char lugares[3];
+    char lugaresMax[3];
+    char lugaresDisponiveis[3];
     char tokey[50];
 
     /* 
@@ -168,16 +189,55 @@ void fromFileToList(Airplane *airplane, char *path){
         depois armazena a struct na lista principal que será manipulada pelo usuário
     */
     while(fgets(tokey, 50, file)){
-        fgets(lugares, 3, file);
+        fgets(lugaresMax, 3, file);
+        fgets(lugaresDisponiveis, 3, file);
         fgets(empresa, 50, file);
-        Data *data = createData(empresa, lugares, tokey);
+        Data *data = createData(empresa, lugaresMax, lugaresDisponiveis, tokey);
         pushToList(airplane, data);
     }
 
     fclose(file);
 }
 
+//gera um tokey aleatorio e garante que nao seja repetido
+char *creatTokey(Airplane *airplane){
+    if(!airplane){
+        perror("erro em creatTokey");
+        exit(1);
+    }
 
+    while(true){
+        char *tokey = malloc(sizeof(char) * 50);
+        int i;
+        for(i = 0; i < 50; i++){
+            tokey[i] = 'A' + rand() % 26;
+        }
+        tokey[i] = '\0';
+
+        if(airplane->size == 0){
+            return tokey;
+        }
+
+        No *current = airplane->ini;
+
+        while(current){
+            if(strcmp(current->data->tokey, tokey) == 0){
+                break;
+            }
+            current = current->next;
+        }
+
+        // current só será diferente de NULL se não tiver estrapolado o final da lista, logo algum aviao com esse tokey
+        if(current){
+            continue;
+        }
+
+        return tokey;
+    }
+
+   
+    
+}
 void fromListToFile(Airplane *airplane, char *path){
 
     if(!airplane){
@@ -193,7 +253,7 @@ void fromListToFile(Airplane *airplane, char *path){
 
     No *current = airplane->ini;
     while(current){
-        fprintf(file, "%s\n%s\n%s\n", current->data->tokey, current->data->lugares, current->data->empresa);
+        fprintf(file, "%s\n%s\n%s\n%s", current->data->tokey, current->data->lugaresMax,current->data->disponiveis, current->data->empresa);
         current = current->next;
     }
     fclose(file);
@@ -213,6 +273,7 @@ int main(){
 
     airplane->ini = NULL;
     airplane->end = NULL;
+    airplane->size = 0;
 
 
     char path[] = "DB/airplaneM.txt";
@@ -220,7 +281,48 @@ int main(){
     fromFileToList(airplane, path);
 
     while(true){
+        printf("Escolha uma opcao:\n");
+        printf("1 - Listar avioes\n");
+        printf("2 - Remover aviao\n");
+        printf("3 - Adicionar aviao\n");
+        printf("4 - Sair\n");
+        
+        char op;
+        
+        scanf("%c", &op);
+        while(getchar() != '\n');
 
+        switch(op){
+            
+            case '1':
+                printList(airplane);
+                break;
+            case '2':
+                
+                break;
+            case '3':
+                char empresa[50];
+                char lugaresMax[] = "50";
+                char lugaresDisponiveis[] = "50";
+                char *tokey = creatTokey(airplane);
+
+                printf("Digite o nome da empresa: ");
+                fgets(empresa, 50, stdin);
+                while(getchar() != '\n');
+
+                Data *data = createData(empresa, lugaresMax, lugaresDisponiveis, tokey);
+                pushToList(airplane, data);
+                free(tokey);
+                break;
+            case '4':
+                fromListToFile(airplane, path);
+                free(airplane);
+                exit(0);
+            default:
+                printf("Opcao invalida\n");
+                break;
+        }
+        
     }
     
 }
