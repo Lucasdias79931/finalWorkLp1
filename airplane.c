40d67348553626e4130fa1f8f1cafbf8d691a6fb
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-12-09 13:40:23 -0300

    ATUALIZANDO AIRPLANE

--- airplane.c
+++ airplane.c
@@ -7,9 +7,9 @@
 
 // Struct para armazenar os dados dos aviões
 typedef struct Data{
-    char *tokey;
-    char *empresa;
-    char *lugaresMax;
+    char tokey[51];
+    char empresa[50];
+    int lugaresMax;
 }Data;
 
 typedef struct No{
@@ -28,8 +28,8 @@ typedef struct Airplane{
 
 // funções para manipular lista de avioes
 
-Data *createData(char *empresa, char *lugaresMax, char *tokey){
-    if(!empresa || !lugaresMax || !tokey ){
+Data *createData(char *empresa, int lugaresMax, char *tokey){
+    if(!empresa ||  !tokey ){
         perror("erro em createData");
         return NULL;
     }
@@ -42,38 +42,11 @@ Data *createData(char *empresa, char *lugaresMax, char *tokey){
         return NULL;
     }
 
-    data->tokey = malloc(strlen(tokey) + 1);
-    if(!data->tokey){
-        perror("erro ao alocar memória");
-        free(data->tokey);
-        free(data);
-        return NULL;
-    }
-
-    data->empresa = malloc(strlen(empresa) + 1);
-    if(!data->empresa){
-        perror("erro ao alocar memória");
-        free(data->empresa);
-        free(data->tokey);
-        free(data);
-        return NULL;
-    }
-
-    data->lugaresMax = malloc(strlen(lugaresMax) + 1);
-    if(!data->lugaresMax){
-        perror("erro ao alocar memória");
-        free(data->lugaresMax);
-        free(data->empresa);
-        free(data->tokey);
-        free(data);
-        return NULL;
-    }
-
     
 
     strcpy(data->tokey, tokey);
     strcpy(data->empresa, empresa);
-    strcpy(data->lugaresMax, lugaresMax);
+    data->lugaresMax = lugaresMax;
     
     return data;
 
@@ -122,9 +95,7 @@ void deleteInList(Airplane *airplane, char *tokey){
             if(airplane->ini == temp){
                 airplane->ini = current;
             }
-            free(temp->data->tokey);
-            free(temp->data->empresa);
-            free(temp->data->lugaresMax);
+            
             free(temp->data);
             free(temp);
             return;
@@ -152,7 +123,7 @@ void printList(Airplane *airplane){
     No *current = airplane->ini;
     while(current){
         printf("\n////////////////////////////////////////////////////////\n");
-        printf("Tokey:%slugaresMax:%sEmpresa:%s", current->data->tokey, current->data->lugaresMax, current->data->empresa);
+        printf("Tokey:%s\nlugaresMax:%i\nEmpresa:%s\n", current->data->tokey, current->data->lugaresMax, current->data->empresa);
         printf("////////////////////////////////////////////////////////\n");
         current = current->next;
     }
@@ -173,20 +144,23 @@ void fromFileToList(Airplane *airplane, char *path){
         return;
     }
 
-    char empresa[50];
-    char lugaresMax[10];
-    char tokey[51];
+    char empresa[51];
+    char lugaresMax[4];
+    char tokey[52];
     /* 
         le os dados do arquivo e armazena em uma struct Data
         depois armazena a struct na lista principal que será manipulada pelo usuário
     */
-    while(fgets(tokey, 51, file) != NULL){
-        fgets(lugaresMax, 10, file);
-        fgets(empresa, 50, file);
-
+    while(fgets(tokey, 52, file) != NULL){
+        fgets(lugaresMax, 4, file);
+        fgets(empresa, 51, file);
         
+        strtok(empresa, "\n");
+        strtok(lugaresMax, "\n");
+        strtok(tokey, "\n");
+        int  maxLugar = atoi(lugaresMax);
         
-        Data *data = createData(empresa, lugaresMax, tokey);
+        Data *data = createData(empresa, maxLugar, tokey);
         pushToList(airplane, data);
     }
 
@@ -194,14 +168,19 @@ void fromFileToList(Airplane *airplane, char *path){
 }
 
 //gera um tokey aleatorio e garante que nao seja repetido
-char *creatTokey(Airplane *airplane){
+char *createTokey(Airplane *airplane){
     if(!airplane){
-        perror("erro em creatTokey");
+        perror("erro em createTokey");
         exit(1);
     }
 
     while(true){
-        char *tokey = malloc(sizeof(char) * 50);
+        char *tokey = malloc(sizeof(char) * 51);
+        if(!tokey){
+            perror("erro ao alocar memória");
+            free(tokey);
+            exit(1);
+        }
         
         for(int i = 0; i < 51; i++){
             tokey[i] = 'A' + rand() % 26;
@@ -247,7 +226,7 @@ void fromListToFile(Airplane *airplane, char *path){
 
     No *current = airplane->ini;
     while(current){
-        fprintf(file, "%s%s%s", current->data->tokey, current->data->lugaresMax, current->data->empresa);
+        fprintf(file, "%s\n%i\n%s\n", current->data->tokey, current->data->lugaresMax, current->data->empresa);
         current = current->next;
     }
     fclose(file);
@@ -294,7 +273,7 @@ int main(){
                 break;
             case '2':
                 printf("Digite o tokey do aviao a ser removido: ");
-                char *tokey = malloc(sizeof(char) * 51);
+                char tokey[51];
                 fgets(tokey, 51, stdin);
                 while(getchar() != '\n');
 
@@ -304,26 +283,29 @@ int main(){
                 scanf("%c", &confirm);
 
                 while(getchar() != '\n');
+
                 if(confirm != 's' && confirm != 'S'){
                     printf("Remocao cancelada\n");
-                    free(tokey);
+                    
                     break;
                 }
                 deleteInList(airplane, tokey);
-                free(tokey);
+                printf("Aviao removido com sucesso\n");
+
                 break;
             case '3':
                 char empresa[50];
-                char lugaresMax[] = "50";
-                tokey = creatTokey(airplane);
+                
+                char *Tokey = createTokey(airplane);
 
                 printf("Digite o nome da empresa: ");
                 fgets(empresa, 50, stdin);
                 while(getchar() != '\n');
 
-                Data *data = createData(empresa, lugaresMax, tokey);
+                Data *data = createData(empresa, 50, Tokey);
                 pushToList(airplane, data);
-                free(tokey);
+                free(Tokey);
+                printf("Aviao adicionado com sucesso\n");
                 break;
             case '4':
                 printf("Saindo sem salvar\n");
